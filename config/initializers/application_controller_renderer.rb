d4806a666b3f1b43e6ff736a50b8df95b34406b1
ryotagonG <ryotapb715@gmail.com>
2021-11-15 09:21:19 +0000

    ユーザー編集実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '525b2a18e16ea6ba88994a028eaae57c93f077efc51116938988b676c3fcb47481e68371b8d251b8eaf7cb82f34f606260617bccc6d4ba7e512303239969c11f'
