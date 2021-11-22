360d85dbaa51867b40c78a35a6f427b70d88b32f
ryotagonG <ryotapb715@gmail.com>
2021-11-22 06:23:17 +0000

    コントローラー記述

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,7 +8,20 @@ class GamesController < ApplicationController
   def new
     @game = Game.new
   end
+  
+  def create
+    @game =Game.new(game_params)
+    @game.user_id = current_user.id
+    @game.save
+    redirect_to game_path(@game)
+  end
 
   def edit
   end
+  
+  private
+  def game_params
+    params.require(:game).permit(:title, :body, :image)
+  end
+  
 end
